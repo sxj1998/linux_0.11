32a0c689e3941bd0315037f849b6770b9e49a428
sxj1998 <19702370@qq.com>
2022-10-17 22:20:35 +0800

    add screen opt

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -2,6 +2,7 @@
 #include <onix/types.h>
 #include <onix/io.h>
 #include <onix/string.h>
+#include <onix/console.h>
 // CRT 地址寄存器 0x3D4
 // CRT 数据寄存器 0x3D5
 // CRT 光标位置 - 高位 0xE
@@ -13,7 +14,7 @@
 // #define CRT_CURSOR_H 0xe
 // #define CRT_CURSOR_L 0xf
 
-char message[] = "hello onix!!!";
+char message[] = "hello onix!!!\n";
 char buf[1024];
 
 void kernel_init()
@@ -28,25 +29,12 @@ void kernel_init()
     // outb(CRT_DATA_REG, 0);
     // outb(CRT_ADDR_REG, CRT_CURSOR_L);
     // outb(CRT_DATA_REG, 0);
-
-    int res;
-    res = strcmp(buf, message);
-    strcpy(buf, message);
-    res = strcmp(buf, message);
-    strcat(buf, message);
-    res = strcmp(buf, message);
-    res = strlen(message);
-    res = sizeof(message);
-
-    char *ptr = strchr(message, '!');
-    ptr = strrchr(message, '!');
-
-    memset(buf, 0, sizeof(buf));
-    res = memcmp(buf, message, sizeof(message));
-    memcpy(buf, message, sizeof(message));
-    res = memcmp(buf, message, sizeof(message));
-    ptr = memchr(buf, '!', sizeof(message));
-
+    
+    console_init();
+    while(true)
+    {
+        console_write(message,sizeof(message) - 1);
+    }
     return;
 
 }
