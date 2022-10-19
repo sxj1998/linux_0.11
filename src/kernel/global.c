4a8235d68d09d8a8e7c11119fc1c87fb7400b665
sxj1998 <19702370@qq.com>
2022-10-19 20:29:40 +0800

    add task

--- src/kernel/global.c
+++ src/kernel/global.c
@@ -9,7 +9,6 @@ pointer_t gdt_ptr;          // 内核全局描述符表指针
 // 初始化内核全局描述符表
 void gdt_init()
 {
-    BMB;
     DEBUGK("init gdt!!!\n");
 
     asm volatile("sgdt gdt_ptr");
@@ -18,7 +17,7 @@ void gdt_init()
 
     gdt_ptr.base = (u32)&gdt;
     gdt_ptr.limit = sizeof(gdt) - 1;
-    BMB;
+
     asm volatile("lgdt gdt_ptr\n");
-    BMB;
+ 
 }
--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,13 +1,20 @@
+
+
+#include <onix/onix.h>
+#include <onix/types.h>
+#include <onix/io.h>
+#include <onix/string.h>
 #include <onix/console.h>
 #include <onix/printk.h>
 #include <onix/assert.h>
 #include <onix/debug.h>
 #include <onix/global.h>
+#include <onix/task.h>
 
 void kernel_init()
 {
     console_init();
     gdt_init();
-
+    task_init();
     return;
 }
