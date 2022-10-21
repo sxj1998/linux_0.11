090c1d70ad2d0694893bc05d36698e5b5d8899df
sxj1998 <19702370@qq.com>
2022-10-21 15:34:51 +0800

    add igdt table

--- src/kernel/start.asm
+++ src/kernel/start.asm
@@ -1,9 +1,14 @@
 [bits 32]
 
 extern kernel_init
+
 global _start
 _start:
     call kernel_init
-    xchg bx,bx
-    int 0x80
-    jmp $
+    xchg bx, bx
+    int 0x80; 调用 0x80 中断函数 系统调用
+
+    ; mov bx, 0
+    ; div bx
+
+    jmp $; 阻塞
