6b845816a377a20608e5968877d7bbec722a9963
sxj1998 <19702370@qq.com>
2022-11-01 16:45:00 +0800

    修改锁机制

--- src/kernel/mutex.c
+++ src/kernel/mutex.c
@@ -64,7 +64,7 @@ void mutex_unlock(mutex_t *mutex)
 
 
 // 自旋锁初始化
-void spin_init(spinlock_t *lock)
+void lock_init(lock_t *lock)
 {
     lock->holder = NULL;
     lock->repeat = 0;
@@ -72,7 +72,7 @@ void spin_init(spinlock_t *lock)
 }
 
 // 尝试持有锁
-void spin_lock(spinlock_t *lock)
+void lock_acquire(lock_t *lock)
 {
     task_t *current = running_task();
     if (lock->holder != current)
@@ -89,7 +89,7 @@ void spin_lock(spinlock_t *lock)
 }
 
 // 释放锁
-void spin_unlock(spinlock_t *lock)
+void lock_release(lock_t *lock)
 {
     task_t *current = running_task();
     assert(lock->holder == current);
